b9e2e525a01a8e58ab52345c921eeaa20e3ae9ff
aldrin163 <aldrinjenson@gmail.com>
2020-01-05 22:48:21 +0530

    Added files to be deployed

---
+++ build/precache-manifest.0a9260ca18cdfe3bf14934f3bbc844f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5abf349e9e66ca559de70025538f208c",
+    "url": "/aldrin163/ToDoApp/index.html"
+  },
+  {
+    "revision": "63d1528283a6637808e5",
+    "url": "/aldrin163/ToDoApp/static/css/main.4df445fb.chunk.css"
+  },
+  {
+    "revision": "abd671a21a1df7954755",
+    "url": "/aldrin163/ToDoApp/static/js/2.e61e5dde.chunk.js"
+  },
+  {
+    "revision": "c7fc72222faf9b567bb59a3bac882c8e",
+    "url": "/aldrin163/ToDoApp/static/js/2.e61e5dde.chunk.js.LICENSE"
+  },
+  {
+    "revision": "63d1528283a6637808e5",
+    "url": "/aldrin163/ToDoApp/static/js/main.e51dc317.chunk.js"
+  },
+  {
+    "revision": "222a28a06d0f9e0a1d87",
+    "url": "/aldrin163/ToDoApp/static/js/runtime-main.778537dd.js"
+  }
+]);
